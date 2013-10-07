6c7b50ddf239ab7605cc901d5d296cbff23908a4
bairyn <ByronJohnsonFP@gmail.com>
2013-10-07 11:45:09 -0700

    Check in Haskell configuration.

---
+++ .cabal/Util.hs
@@ -0,0 +1,36 @@
+--{-# OPTIONS_GHC -interactive-print=myPrint #-}  -- This doesn't work; added
+--it to alias in .zshrc.
+{-# LANGUAGE Arrows #-}
+module Util where
+
+import Prelude hiding (id, (.))
+import Control.Arrow
+import Control.Category
+import Control.Monad
+import System.Random
+-- http://stackoverflow.com/questions/5535512/how-to-hack-ghci-or-hugs-so-that-it-prints-unicode-chars-unescaped
+import System.IO
+import qualified Text.PrettyPrint.ANSI.Leijen as PP
+
+-- Very inefficiently scramble a list.
+scramble :: [a] -> StdGen -> ([a], StdGen)
+scramble xs s = case length xs of
+    0 -> (xs, s)
+    n -> let (currentElemIndex, s') = randomR (0, pred n) s
+             (left, right) = splitAt currentElemIndex xs
+         in  first (head right :) $ scramble (left ++ tail right) s'
+
+scrambleIO :: [a] -> IO [a]
+scrambleIO xs = do
+    s <- getStdGen
+    let (xs', s') = scramble xs s
+    setStdGen s'
+    return xs'
+
+-- | Shuffle the words in a string and print the result.
+shuffleWords :: String -> IO ()
+shuffleWords = putStrLn . unwords <=< scrambleIO . words
+
+-- http://stackoverflow.com/questions/5535512/how-to-hack-ghci-or-hugs-so-that-it-prints-unicode-chars-unescaped
+myPrint :: (PP.Pretty a) => a -> IO ()
+myPrint a = (PP.hPutDoc stdout . PP.pretty) a >> putStrLn ""
